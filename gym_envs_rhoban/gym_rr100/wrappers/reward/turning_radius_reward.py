01d1cb0dc93d081e8e353403d89c1b8c0f5ad1c5
Kenobi <kenobi@rhoban>
2025-09-15 17:08:30 +0200

    Finish implementing TurningRadiusReward

--- gym_envs_rhoban/gym_rr100/wrappers/reward/turning_radius_reward.py
+++ gym_envs_rhoban/gym_rr100/wrappers/reward/turning_radius_reward.py
@@ -10,16 +10,17 @@ class TurningRadiusReward(RewardWrapper):
     def __init__(self, env, min_turning_radius: float = 0.5, penalty: float = 5.0):
         super().__init__(env)
         self.min_turning_radius = min_turning_radius
+        self.penalty = penalty
 
     def reward(self, reward: float) -> float:  # type: ignore
-        penalty = 0.0
         goal_in_robot_frame = np.array(self.env.unwrapped._get_position_in_robot_frame(self.env.unwrapped.goal))  # type: ignore
         dist_left = np.linalg.norm(
-            goal_in_robot_frame - np.array([0, self.min_turning_radius + self.env.unwrapped.distance_threshold])
+            goal_in_robot_frame - np.array([0, self.min_turning_radius])
         )
         dist_right = np.linalg.norm(
-            goal_in_robot_frame - np.array([0, -self.min_turning_radius - self.env.unwrapped.distance_threshold])
+            goal_in_robot_frame - np.array([0, -self.min_turning_radius])
         )
-        if dist_left < self.min_turning_radius or dist_right < self.min_turning_radius:
-            penalty = 2.0
+        penalty = 0.0
+        if dist_left < (self.min_turning_radius - self.env.unwrapped.distance_threshold) or dist_right < (self.min_turning_radius - self.env.unwrapped.distance_threshold):
+            penalty = self.penalty
         return reward - penalty
